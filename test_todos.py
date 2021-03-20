7d3c438f3786982d7faba7aa30cd068f1936c60f
TThanushan <thanushantharmabalan@gmail.com>
2021-03-20 18:07:21 +0100

    test_parse_delete() test added.
    test_parse_delete() test added.

--- test_todos.py
+++ test_todos.py
@@ -20,8 +20,27 @@ def test_task_manager_starts_with_no_tasks():
 def test_parse_add():
     command = "+ first task"
     task_manager = TaskManager()
-
     action = task_manager.parse(command)
 
     assert action.name == "add"
     assert action.description == "first task"
+
+
+def test_parse_delete():
+    # Adding the task.
+    command = "+ first task"
+    task_manager = TaskManager()
+    action = task_manager.parse(command)
+
+    task_manager.parse_logic(action)
+    task_added = task_manager.tasks[0]
+
+    # Remove the task.
+    command = "- first task"
+    action = task_manager.parse(command)
+    task_manager.parse_logic(action)
+
+    # Check if task has been deleted.
+    for _task in task_manager.tasks:
+        if _task.id == task_added.id:
+            AssertionError
--- todos.py
+++ todos.py
@@ -1,9 +1,67 @@
 class TaskManager:
+    tasks = []
+    global_task_id = 0
+
+    def main(self):
+        print("Aucune tâche pour le moment")
+
+        command = ""
+        while command != "exit":
+            command = input()
+            action = self.parse(command)
+            self.parse_logic(action)
+
     def parse(self, command):
-        return Action("add", command[2:])
+        word = ""
+
+        if command[0] == "+":
+            word = "add"
+        elif command[0] == "-":
+            word = "delete"
+        elif command[0] == "x":
+            word = "done"
+        elif command == "0":
+            word = "todo"
+
+        description = command[2:]
+        action = Action(word, description)
+        return action
+
+    def parse_logic(self, action):
+        if action[0] == "add":
+            self.tasks.append(action)
+        elif action[0] == "delete":
+            self.delete_task(action)
+        elif action[0] == "done":
+            self.tasks.append(action)
+        elif action == "todo":
+            self.tasks.append(action)
+
+    def add_task(self, task):
+        self.tasks.append(task)
+        self.global_task_id += 1
+
+    def delete_task(self, task):
+        i = 0
+        for _task in self.tasks:
+            if _task.id == task.id:
+                del self.tasks[i]
 
 
 class Action:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+
+class Task:
+    def __init__(self, id, description):
+        self.description = description
+        self.id = id
+        self.status = "à faire"
+
+    def done(self):
+        self.status = "fait"
+
+    def todo(self):
+        self.status = "à faire"
